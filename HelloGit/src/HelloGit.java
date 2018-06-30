f604157b3fe7e90eb1e392655223495ef939c622
bhilal84 <bhilal_84@yahoo.com>
2018-06-30 19:05:18 +0700

    new change

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -3,6 +3,6 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello git!");
-		System.out.println("How are you?");
+		System.out.println("How are you? Fine!");
 	}
 }
